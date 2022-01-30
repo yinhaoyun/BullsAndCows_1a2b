5a2e490fab6fff24adb427635738ef509ddbb9f6
Hao-Yun Yin <haoyun.tw@gmail.com>
2022-01-30 16:25:00 +0800

    initial of Wordle Solver

--- main.py
+++ main.py
@@ -1,31 +1,23 @@
+import random
+from collections import defaultdict
 from typing import List
 
 
-def generate_ans() -> List[str]:
-    ans = []
-    formed = []
-    used = [False] * 10
+WORD_LENGTH = 5
+COMPUTER, USER = range(2)
+DICT_FILE = "words.txt"
 
-    def backtrack() -> None:
-        if len(formed) == 4:
-            ans.append(list(formed))
-            return
-        for i in range(len(used)):
-            if used[i]:
-                continue
-            used[i] = True
-            formed.append(i)
-            backtrack()
-            formed.pop()
-            used[i] = False
 
-    backtrack()
-    return ans
+def generate_word_list() -> List[str]:
+    file = open(DICT_FILE, 'r')
+    word_list = file.readlines()
+    word_list = [w.strip().upper() for w in word_list]
+    return word_list
 
 
 def parse_ab(user_input) -> tuple[int, int]:
     user_input = user_input.strip()
-    if len(user_input) != 4:
+    if len(user_input) != 5:
         return -1, -1
     if user_input[1] != "a" or user_input[3] != "b":
         return -1, -1
@@ -34,38 +26,78 @@ def parse_ab(user_input) -> tuple[int, int]:
     return a, b
 
 
-def verify_ab(ans1: List[int], ans2: List[int]) -> tuple[int, int]:
-    a, b = 0, 0
-    for i, n in enumerate(ans1):
-        if ans1[i] == ans2[i]:
-            a += 1
-        elif n in ans2:
-            b += 1
-    return a, b
+def verify_ab(word1: str, word2: str) -> str:
+    res = [0] * WORD_LENGTH
+    for i in range(WORD_LENGTH):
+        if word1[i] == word2[i]:
+            res[i] = 2
+        elif word2[i] in word1:
+            res[i] = 1
+
+    return "".join([str(r) for r in res])
 
 
-def delete_mismatch(ans, guess, a, b) -> List[List[int]]:
-    def match(ans: List[int]) -> bool:
-        va, vb = verify_ab(ans, guess)
-        return va == a and vb == b
+def pre_process_response(guess: str, response: str) -> str:
+    # Special Rule:
+    # 1. "CHURR": 00210 should be 00211" because the second "R" occur once.
+    # 2. "CHURR": 00020 should be 00021" because the second "R" occur once.
+    # Simple by: if a character occurs twice, it is impossible to be 0, fix it to 1
+    index_score_map = defaultdict(list)
+    response = [int(score) for score in response]
+    for i, c in enumerate(guess):
+        index_score_map[c].append((i, response[i]))
+    for c in index_score_map:
+        if len(index_score_map[c]) > 1:
+            if any([score for index, score in index_score_map[c]]):
+                for index, score in index_score_map[c]:
+                    if score == 0:
+                        response[index] = 1
+    return "".join(str(score) for score in response)
 
-    return list(filter(match, ans))
+
+def delete_mismatch(answers: List[str], guess: str, response: str) -> List[str]:
+    response = pre_process_response(guess, response)
+    print(f"delete_mismatch: pre_process_response={response}")
+
+    def match(word: str) -> bool:
+        nonlocal response, guess
+        return verify_ab(word, guess) == response
+
+    return list(filter(match, answers))
 
 
 if __name__ == '__main__':
-    ans = generate_ans()
-    while len(ans) > 1:
-        print(ans)
-        print(len(ans))
-        print(f"And I guess {ans[0]}")
-        user_input = input("?a?b: ")
-        a, b = parse_ab(user_input)
-        if a < 0 or b < 0:
-            print("Invalid answer, input again...")
+    word_list = generate_word_list()
+    print(f"Instruction")
+    print(f" * Use the suggestion and feedback the response by 5 digits - 0:gray,1:yellow,2:green, E.g. '00120'")
+    print(f" * Use your own answer and feedback the response, just type your answer")
+    print(f"   Then type the response. E.g. '00120")
+    print(f" * If the answer is not in the word list (Unable to guess), type '33333'")
+    while len(word_list) > 1:
+        print(word_list)
+        print(f"The current number of word_list: {len(word_list)}")
+        guess_i = random.randint(0, len(word_list) - 1)
+        guess_word = word_list[guess_i]
+        guesser = COMPUTER
+        while True:
+            print(f"{'COMPUTER' if guesser == COMPUTER else 'USER'} guess \"{guess_word}\"")
 
-        if a + b > 4:
-            print("a + b must less or equal to 4")
+            user_input = input("Type the feedback or your answer: ")
+            if len(user_input) == 5:
+                if user_input.isalpha():
+                    guesser = USER
+                    guess_word = user_input.upper()
+                    continue
+                if user_input == "33333":
+                    word_list.remove(guess_word)
+                    break
+                if user_input.count("0") + user_input.count("1") + user_input.count("2") == WORD_LENGTH:
+                    word_list = delete_mismatch(word_list, guess_word, user_input)
+                    break
 
-        ans = delete_mismatch(ans, ans[0], a, b)
-
-    print(f"The answer is {ans[0]}")
+            # Exception Handle
+            print(f"Please type 5 digits or 5 letters...")
+    if len(word_list) == 1:
+        print(f"The answer is {word_list}")
+    else:
+        print(f"WTF...I cannot guess the answer!")
